ba31a1e8af966c0e201bbd14955eb0e690001dc1
ttbb <apples398@gmail.com>
2026-04-24 22:20:29 +0800

    Add SHA256 checksums for tui-v0.2.8

--- Formula/proxybot-tui.rb
+++ Formula/proxybot-tui.rb
@@ -4,15 +4,16 @@ class ProxybotTui < Formula
   license "MIT"
   version "v0.2.8"
 
-  if OS.mac? && Hardware::CPU.type == :arm64
+  if Hardware::CPU == :arm64
     url "https://github.com/mbpz/proxybot/releases/download/tui-v0.2.8/proxybot-tui-arm64"
-    sha256 ""
-  elsif OS.mac? && Hardware::CPU.type == :intel
+    sha256 "28bcd8216f22578ee1578a94dad37881736d3efd6337a093716c3473fa428a9b"
+  else
     url "https://github.com/mbpz/proxybot/releases/download/tui-v0.2.8/proxybot-tui-x86_64"
-    sha256 ""
+    sha256 "048c00b7a83ac09865523f6ccaec44237ef1f289037aa15c9b233b32e38165bc"
   end
 
   def install
-    bin.install "proxybot-tui" => "proxybot-tui"
+    bin.install "proxybot-tui-arm64" => "proxybot-tui" rescue nil
+    bin.install "proxybot-tui-x86_64" => "proxybot-tui" rescue nil
   end
 end
