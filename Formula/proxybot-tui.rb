2981c1d865c00775f763a24d2161c15e6d1b077f
ttbb <apples398@gmail.com>
2026-04-24 16:48:45 +0800

    Simplify formula conditional

--- Formula/proxybot-tui.rb
+++ Formula/proxybot-tui.rb
@@ -4,10 +4,10 @@ class ProxybotTui < Formula
   license "MIT"
   version "v0.2.7"
 
-  if OS.mac? && Hardware::CPU.type == :arm64
+  if Hardware::CPU == :arm64
     url "https://github.com/mbpz/proxybot/releases/download/tui-v0.2.7/proxybot-tui-arm64"
     sha256 "28bcd8216f22578ee1578a94dad37881736d3efd6337a093716c3473fa428a9b"
-  elsif OS.mac? && Hardware::CPU.type == :intel
+  else
     url "https://github.com/mbpz/proxybot/releases/download/tui-v0.2.7/proxybot-tui-x86_64"
     sha256 "048c00b7a83ac09865523f6ccaec44237ef1f289037aa15c9b233b32e38165bc"
   end
