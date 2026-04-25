f50a17ff2028b5278b11a432d9ee236376a773d7
mbpz <apples398@163.com>
2026-04-25 18:07:48 +0800

    fix: add sha256 hashes for tui-v0.3.0

--- Formula/proxybot-tui.rb
+++ Formula/proxybot-tui.rb
@@ -6,10 +6,10 @@ class ProxybotTui < Formula
 
   if OS.mac? && Hardware::CPU.type == :arm64
     url "https://github.com/mbpz/proxybot/releases/download/tui-v0.3.0/proxybot-tui-arm64"
-    sha256 ""
+    sha256 "27af394f03678b3a9ee8644a1a2150789edd46789e8784d0161d600b20193940"
   elsif OS.mac? && Hardware::CPU.type == :intel
     url "https://github.com/mbpz/proxybot/releases/download/tui-v0.3.0/proxybot-tui-x86_64"
-    sha256 ""
+    sha256 "6bb91c3e69bcfd5b3b514068ef094c881dafe0502a9d35d38f92747b3e8de848"
   end
 
   def install
