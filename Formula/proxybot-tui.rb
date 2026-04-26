19569ce70331049379d7142e9f6a6ad9a825a6ca
ttbb <apples398@gmail.com>
2026-04-26 08:29:18 +0800

    Update Homebrew formula to tui-v0.3.1

--- Formula/proxybot-tui.rb
+++ Formula/proxybot-tui.rb
@@ -2,14 +2,14 @@ class ProxybotTui < Formula
   desc "ProxyBot TUI — HTTPS MITM proxy terminal UI"
   homepage "https://github.com/mbpz/proxybot"
   license "MIT"
-  version "v0.3.0"
+  version "v0.3.1"
 
   if OS.mac? && Hardware::CPU.type == :arm64
-    url "https://github.com/mbpz/proxybot/releases/download/tui-v0.3.0/proxybot-tui-arm64"
-    sha256 "27af394f03678b3a9ee8644a1a2150789edd46789e8784d0161d600b20193940"
+    url "https://github.com/mbpz/proxybot/releases/download/tui-v0.3.1/proxybot-tui-arm64"
+    sha256 ""
   elsif OS.mac? && Hardware::CPU.type == :intel
-    url "https://github.com/mbpz/proxybot/releases/download/tui-v0.3.0/proxybot-tui-x86_64"
-    sha256 "6bb91c3e69bcfd5b3b514068ef094c881dafe0502a9d35d38f92747b3e8de848"
+    url "https://github.com/mbpz/proxybot/releases/download/tui-v0.3.1/proxybot-tui-x86_64"
+    sha256 ""
   end
 
   def install
